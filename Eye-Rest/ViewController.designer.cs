3c6fe600ef50fd9f251c437cbb6bd4c8dca4b1dd
Yevhen Voitiuk <yevhen.voitiuk447@myci.csuci.edu>
2021-02-02 22:44:12 -0800

    Edited simple label and button in Designer

--- Eye-Rest/ViewController.designer.cs
+++ Eye-Rest/ViewController.designer.cs
@@ -14,5 +14,34 @@ namespace Eye_Rest
     [Register ("ViewController")]
     partial class ViewController
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Funny Booton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel Welcome Label { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView Welcome Label { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (Funny Booton != null) {
+                Funny Booton.Dispose ();
+                Funny Booton = null;
+            }
+
+            if (Welcome Label != null) {
+                Welcome Label.Dispose ();
+                Welcome Label = null;
+            }
+
+            if (Welcome Label != null) {
+                Welcome Label.Dispose ();
+                Welcome Label = null;
+            }
+        }
     }
 }
